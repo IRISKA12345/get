736b5e949234ee076c99b328673d6222d2774638
IRINA <i5bitneva@yandex.ru>
2026-02-25 13:10:03 +0300

    Исправление кода

--- get-dac/8-bit-dac-manual.py
+++ get-dac/8-bit-dac-manual.py
@@ -3,7 +3,7 @@ import time
 
 print()
 
-dac_bins = [16,20,21,25,26,17,27,22]
+dac_pins = [16,20,21,25,26,17,27,22]
 
 GPIO.setmode(GPIO.BCM)
 for pin in dac_pins:
@@ -13,7 +13,7 @@ V_REF = 3.3
 
 def voltage_to_number(voltage):
     if not (0.0 <=voltage<=V_REF):
-        print(f"Нвпряжение выходит за динамический диапазон ЦАП")
+        print(f"Напряжение выходит за динамический диапазон ЦАП")
         print("Устанавливаю 0.0 В")
         return 0
     return int(voltage / V_REF * 255)
@@ -21,7 +21,7 @@ def voltage_to_number(voltage):
 def number_to_dac(number):
     for i in range(8):
         bit = (number>>(7-i)) & 1
-        GPIO.output(dac_bits[i],bit)
+        GPIO.output(dac_pins[i],bit)
 
 GPIO.setmode(GPIO.BCM)
 for pin in dac_pins:
@@ -30,13 +30,13 @@ for pin in dac_pins:
 try:
     while True:
         try:
-            voltare = float(input("Введите напряжение в Вольтах:"))
-            number = voltare_to_number(voltage)
+            voltage = float(input("Введите напряжение в Вольтах:"))
+            number = voltage_to_number(voltage)
             number_to_dac(number)
 
         except ValueError:
             print("Вы не ввели число.Попробуйте еще раз.")
 
 finally:
-    GPIO.output(dac_bits, 0)
+    GPIO.output(dac_pins, 0)
     GPIO.cleanup()
